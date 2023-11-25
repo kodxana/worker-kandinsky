a49843c550ee7432ce85c3ff14c215f1050daac8
kodxana <romanrex19@int.pl>
2023-11-25 13:00:44 +0100

    code update

--- builder/cache_models.py
+++ builder/cache_models.py
@@ -3,14 +3,17 @@ Fetches and caches the Kandinsky models.
 '''
 
 import torch
-from diffusers import KandinskyV3Pipeline
+from huggingface_hub import hf_hub_download
+from kandinsky3 import get_T2I_unet, get_T5encoder, get_movq, Kandinsky3T2IPipeline
 
 def get_kandinsky_pipelines():
     # Kandinsky 3 pipelines
-    pipe_prior_3 = KandinskyV3Img2ImgPipeline.from_pretrained(
-        "kandinsky-community/kandinsky-3", torch_dtype=torch.float16)
+    unet_path = hf_hub_download(
+        repo_id="ai-forever/Kandinsky3.0", filename='weights/kandinsky3.pt')
+    movq_path = hf_hub_download(
+          repo_id="ai-forever/Kandinsky3.0", filename='weights/movq.pt')
 
-    return (pipe_prior_3)
+    return (unet_pat, movq_path)
 
 if __name__ == "__main__":
     get_kandinsky_pipelines()
